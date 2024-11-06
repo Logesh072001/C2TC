a755ca63a0f26e6fe9d0fcc4ba7c7a02b3b00ad8
loges <loges@LAPTOP-4STCTIMA>
2024-11-06 12:42:37 +0530

    jj

---
+++ C2TC_LOGESH/src/com/tns/ifet/day/four/FinalMethodClassTest.java
@@ -0,0 +1,11 @@
+package com.tns.ifet.day.four;
+
+public class FinalMethodClassTest {
+	 // Compile time error because we cannot override the final method 
+	/*
+	 * @Override void show() {
+	 * 
+	 * System.out.println("This is the final method of FinalMethodEx class"); }
+	 */
+   
+}
